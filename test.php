23f7f99ff7e83dc66dbcd9734b3ab5f21d16104a
thienhustk64 <ducthien1005@gmail.com>
2022-01-11 21:06:34 +0700

    Thien
    test code php

--- test.php
+++ test.php
@@ -2,6 +2,12 @@
     $cn = pg_connect( "host=ec2-23-23-199-57.compute-1.amazonaws.com port=5432 dbname=d5voph6nh682ao
     user=xmttvyrbszbnze password=c467c601c9d08db139aacd4bc1e6148efa419eebc6ac5dc377b828673ffb463e");
     if( $cn){
-        echo "connected"
+        echo 'connected';
     }
+
+    $result = pg_query( $cn, 'select * from "WareHouse"');
+    while ( $row = pg_fetch_object( $result)){
+        echo "<br/>".$row->ID_Product;
+    }
+    pg_close( $cn);
 ?>
