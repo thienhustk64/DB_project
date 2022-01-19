cbc174d96b95e2eecf48908d5158ea5384b85e69
SstrikesS <thanhnguyentb2001@gmail.com>
2022-01-19 21:09:06 +0700

    thanh
    adaskdkal

--- main_pages/header.php
+++ main_pages/header.php
@@ -1,16 +1,23 @@
 <div class="header">
-
+    <div class ="logo">
+        <a href= "index.php">
+            <img src="../images/logo/logo.jpg">
+        </a>
+    </div>
             <ul class ="list_main_menu">
                 <li><a href="index.php?quanly=WareHouse">Trang chủ</a></li>
                 <li><a href="index.php?quanly=WareHouse">Kho hàng</a></li>
                 <li><a href="index.php?quanly=Import">Nhập lô</a></li>
                 <li><a href="index.php?quanly=Export">Xuất kho</a></li>
             </ul>
-            <p>
+            <div class = "box">
+                <div class= "clear"></div>
                 <form class="timkiem" action="index.php?quanly=findtool" method="POST" accept-charset="utf-8">
-                    <input class="timkiem_txt" type="text" placeholder="Tìm kiếm sản phẩm" name="tukhoa"></input>
-                    <input class="timkiem_but" type="submit" name="findtool" value="Tìm kiếm"></input>
+                    <input class="timkiem_txt" type="text" placeholder="Tìm kiếm sản phẩm" name="tukhoa" required></input>
+                    <input class "sbutton" type ="submit" name = "findtool" value ="Tìm">
+                    </input>
                 </form>
-            </p> 
+            </div>
+    <div class ="clear"></div> 
 </div>
 
